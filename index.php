693a9018379602c7f6b8f3ace3dde9606b292a3c
Zyon Dias <zyon.dias@hotmail.com>
2013-05-28 17:36:03 -0300

    acresetando formulario

--- index.php
+++ index.php
@@ -2,28 +2,39 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/css.css" rel="stylesheet" type="text/css">
         <title></title>
     </head>
     <body>
+        <form >
+            <label>Full name:</label>
+            <input type="text" id="full_name" name="full_name" placeholder="Jane Doe" required>
+
+            <label>Email address:</label>
+            <input type="email" id="email_addr" name="email_addr" required>
+
+            <label>Repeat email address:</label>
+            <input type="email" id="email_addr_repeat" name="email_addr_repeat" required 
+                   oninput="check(this)">
+
+           <!-- <label>Arrival date:</label>
+            <input type="date" id="arrival_dt" name="arrival_dt" required>-->
+
+            <input type="submit" value="Gravar" /> 
+        </form>
+
+        <script>
+            function check(input) {
+                if (input.value != document.getElementById('email_addr').value) {
+                    input.setCustomValidity('The two email addresses must match.');
+                } else {
+                    // input is valid -- reset the error message
+                    input.setCustomValidity('');
+                }
+            }
+        </script>
+
         <?php
-        //manipulando array
-        $pessoa =  array(
-                                            'Marina'=>  array('cabelo'=>'loiro', 'sexo'=>'Mulher', 'formacao'=>'superior'),
-                                            'Danielle'=>array('cabelo'=>'Moreno','sexo'=>'Mulher'),
-                                            'Fernando'=>array('cabelo'=>'Enrolado','sexo'=>'Homen')
-                                    );
-        //imprimi a estrutura do array
-        var_dump($pessoa);
-        
-                    foreach($pessoa as $nomes =>$caracteristicas)
-                    {
-                        echo "Quemm:  $nomes <br>\n";
-                        foreach($caracteristicas as $caracteristicas => $valor)
-                        {
-                            echo "$caracteristicas -> $valor <br>";
-                        }
-                        echo "<br><br>";
-                    }    
         //includndo a class
         include_once 'class/Escola.php';
         include_once 'class/Aluno.php';
@@ -31,19 +42,18 @@
         $Educacao = new Escola();
         $Educacao->setNomeEscola("E.E MARIA CLARA DE OLIVEIRA");
         $Educacao->setInep(8920);
-        
+
         echo $Educacao->inep;
         echo $Educacao->nomeEscola;
-        
+
         $Educacao->cadastraEscola();
-        
+
         $aluno = new Aluno($Educacao);
         $aluno->setNome("   Fernandodinhooo   -   ");
-        echo  $aluno->toString();
-        
+        echo $aluno->toString();
         ?>
-        
-        
-        
+
+
+
     </body>
 </html>
